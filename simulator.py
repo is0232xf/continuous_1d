a509524c0914c008e4633448227e004a9b66cfe2
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-09 17:13:40 +0900

    状態遷移と観測のモデルを追加した

--- simulator.py
+++ simulator.py
@@ -5,6 +5,42 @@ Created on Fri Sep 09 16:09:40 2016
 @author: Fujiichang
 """
 
+import random
+
+
+class Simulator(object):
+    def __init__(self):
+        self._s = 0
+        self.sigma = 0.1
+        self.landmark = [0, 1, 2, 3, 4]
+
+    def _draw_s(self, actual_a):
+        new_s = self._s + actual_a
+        return new_s
+
+    def get_o(self):
+        o = 5 * [0]
+        for i in range(len(self.landmark)):
+            mu = self.landmark[i] - self._s
+            o[i] = random.gauss(mu, self.sigma)
+        return o
+
+    def set_a(self, a):
+        actual_a = random.gauss(a, self.sigma)
+        self._s = self._draw_s(actual_a)
+
+    def get_s(self):
+        return self._s
+
+
 if __name__ == "__main__":
+    simulator = Simulator()
+    a = 1
 
+    for i in range(4):
+        simulator.set_a(a)
+        s = simulator.get_s()
+        o = simulator.get_o()
 
+        print "s :", s
+        print "o :", o
